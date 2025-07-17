b213d16f38a8856ca55079fe8f9843223f2eb6d4
nh <nichohuaman16@gmail.com>
2025-07-17 04:42:38 -0500

    refactor: organize and optimize project controller

--- src/controllers/ProjectController.ts
+++ src/controllers/ProjectController.ts
@@ -5,22 +5,26 @@ export class ProjectController {
 
     static getAllProjects = async(req: Request, res:Response) => {
         try {
-            const projects = await Project.find().populate('tasks'); // Populate tasks field
-            res.status(200).json(projects);
+            // Retrieve all projects and populate their associated tasks
+            const projects = await Project.find().populate('tasks');
+            return res.status(200).json({
+                message: 'Projects fetched successfully',
+                data: projects
+            })
         } catch (error) {
-            res.status(500).json({ message: 'Error fetching projects', error });
+            return res.status(500).json({ message: 'Error fetching projects', error });
         }
     } 
 
     
     static getProjectById = async(req: Request, res: Response) => {
         try {
-            const id = req.params.id;
+            const { id } = req.params;
             const projectById = await Project.findById(id).populate('tasks');
             if(!projectById) {
                 return res.status(404).json({ error: 'Project not found' });
             }
-            res.status(200).json(projectById);
+            return res.status(200).json(projectById);
         } catch (error) {
             res.status(500).json({ message: 'Error fetching project by ID', error });
         }
@@ -29,8 +33,12 @@ export class ProjectController {
     static createProject = async(req: Request, res: Response) => {
         const project = new Project(req.body);
         try {
-            await project.save();
-            res.status(201).send({ message: 'Project created successfully' });
+            const saveProject = await project.save();
+
+            return res.status(201).json({
+                message: 'Project created successfully',
+                data: saveProject
+            });
         } catch (error) {
             res.status(500).json({ message: 'Error creating project', error });
         }
@@ -38,16 +46,22 @@ export class ProjectController {
 
     static updateProjectById = async(req: Request, res: Response) => {
         try {
-            const id = req.params.id;
-            const project = await Project.findByIdAndUpdate(id,req.body);
-            if(!project) {
-                const error = new Error('Project not found');
-                return res.status(404).json({ error: error.message });
+            const { id } = req.params;
+            const updatedProject = await Project.findByIdAndUpdate(id, req.body, {
+                new: true,
+                runValidators: true,
+            });
+
+            if (!updatedProject) {
+                return res.status(404).json({ message: 'Project not found' });
             }
-            await project.save();
-            res.status(200).send({ message: 'Project updated successfully' });
+
+             return res.status(200).json({
+                message: 'Project updated successfully',
+                data: updatedProject,
+            });
         } catch (error) {
-            res.status(500).json({ message: 'Error updating project', error });
+            return res.status(500).json({ message: 'Error updating project', error });
         }
     }
 
@@ -59,12 +73,11 @@ export class ProjectController {
             if (!project) {
                 return res.status(404).json({ message: 'Project not found' });
             }
-            await Project.deleteOne();
-            res.status(200).send({ message: 'Project deleted successfully' });
+            await Project.deleteOne({ _id: id });
+            return res.status(200).json({ message: 'Project deleted successfully' });
         } catch (error) {
-            res.send({ message: 'Error deleting project', error });
+            return res.send({ message: 'Error deleting project', error });
         }
     }
-
 
 }
