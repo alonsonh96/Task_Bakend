d959b15d7a3c24efa90306147fc09be37627bc74
nh <nichohuaman16@gmail.com>
2025-10-11 21:45:19 -0500

    fix: adjust swagger.yaml path and copy to dist/docs

--- src/docs/swagger.ts
+++ src/docs/swagger.ts
@@ -38,6 +38,6 @@ const swaggerOptions: swaggerJSDoc.Options = {
 
 
 export async function setupSwagger(app: import('express').Express) {
-  const swaggerDoc = await SwaggerParser.dereference(path.join(__dirname, './swagger.yaml'));
+  const swaggerDoc = await SwaggerParser.dereference(path.join(__dirname, 'docs', 'swagger.yaml'));
   app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 }
